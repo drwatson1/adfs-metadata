c13ad58dbbc240c6aeff04ecc8208b2c6243ec7b
Sergey Tregub <drwatson1@yandex.ru>
2017-06-07 10:27:38 +0300

    Unit test was added

---
+++ src/DrWatson.Adfs.Metadata.Test/ParserTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DrWatson.Adfs.Metadata.Test
+{
+    [TestClass]
+    public class ParserTest
+    {
+        private string GroomedValidMetadataPath { get => @"D:\GitHub\adfs-metadata\src\DrWatson.Adfs.Metadata.Test\TestData\GroomedValidFederationMetadata.xml"; }
+        private string GroomedValidMetadata { get => File.ReadAllText(GroomedValidMetadataPath); }
+
+        [TestMethod]
+        public void ParserShouldNotThrowOnValidMetadata()
+        {
+            Action act = () => new Parser(GroomedValidMetadata);
+
+            act.ShouldNotThrow();
+        }
+    }
+}
