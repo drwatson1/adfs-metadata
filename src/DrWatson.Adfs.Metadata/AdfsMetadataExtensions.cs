7df5884e7deb65c26ef8f2f2dbcc10f475104d23
Sergey Tregub <drwatson1@yandex.ru>
2018-02-12 14:59:12 +0300

    Extension methods are added to simplify register metadata loader as a service in the IServiceCollection

--- src/DrWatson.Adfs.Metadata/AdfsMetadataExtensions.cs
+++ src/DrWatson.Adfs.Metadata/AdfsMetadataExtensions.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace DrWatson.Adfs.Metadata
 {
     public static class AdfsMetadataExtensions
     {
         public static X509Certificate2 GetSigningCertificate(this AdfsMetadata obj) => new X509Certificate2(Convert.FromBase64String(obj.SigningCertificateString));
+
+        public static IServiceCollection AddAdfsMetadata(this IServiceCollection services, string adfsBaseUrl)
+        {
+            services.AddTransient<AdfsMetadataService>(provider => new AdfsMetadataLoader(adfsBaseUrl));
+            return services;
+        }
+
+        public static IServiceCollection AddAdfsMetadata(this IServiceCollection services, Func<Task<string>> loader)
+        {
+            services.AddTransient<AdfsMetadataService>(provider => new AdfsMetadataLoader(loader));
+            return services;
+        }
     }
 }
